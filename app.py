500df6a9041e964894c635a726676a3d7fdf6783
sambhav <sambhav.reddy@gmail.com>
2017-12-18 21:32:54 -0800

    v1.2.1

--- app.py
+++ app.py
@@ -64,10 +64,8 @@ def run_loop(img):
 ###
 #Flask
 ###
-UPLOAD_FOLDER = 'path/uploads'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 user_folder = ""
 
 def allowed_file(filename):
